dcd585ac1e35dfb7720d9dec54ddde281b1d1ec7
Josh Seckman <jseckman90@gmail.com>
2021-01-08 19:55:31 -0500

    mongodb connected

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 const transactions = require("./routes/transactions");
 
 dotenv.config({ path: "./config/config.env" });
+connectDB();
 
 PORT = process.env.PORT || 4000;
 const app = express();
